ce4148357a36a6dc8c7caf4644853e87875aae85
Phong Van Nguyen <phong_nguyen@hcl.com>
2021-02-12 22:55:39 -0500

    initial commit

---
+++ src/test/java/com/phase3section3project/Phase2EndProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phase3section3project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phase2EndProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
